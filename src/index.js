72be296f54044ff832a0f4cce6ac0ef862ae5217
SeratiVilankulu <seratimotla@gmail.com>
2026-04-24 13:01:05 +0200

    adding small different  components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <div>Hello React!</div>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/salamino.jpg" alt="Pizza Salamino" />
+      <h2>Pizza Salamino</h2>
+      <p>Tomato, mozarella, and pepperoni</p>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
